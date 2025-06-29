154ab7fb6e5005a7d20ec7e2f7cf03c0c2826a2e
htande <htande@hotmail.fr>
2025-06-29 16:10:52 +0200

    test(core): basic integration tests for SensorData and TangleBlock

---
+++ tests/core_tests.rs
@@ -0,0 +1,31 @@
+use ecoblock_core::{SensorData, NodeId, TangleBlock, Signature};
+
+#[test]
+fn sensor_data_can_be_created() {
+    let data = SensorData {
+        pm25: 12.3,
+        co2: 415.0,
+        temperature: 22.5,
+        humidity: 45.0,
+        timestamp: 1_694_888_000,
+    };
+
+    assert_eq!(data.pm25, 12.3);
+    assert!(data.co2 > 400.0);
+}
+
+#[test]
+fn tangle_block_construction() {
+    let node_id = NodeId("abc123".into());
+    let signature = Signature("signed".into());
+    let data = SensorData {
+        pm25: 10.0,
+        co2: 400.0,
+        temperature: 20.0,
+        humidity: 50.0,
+        timestamp: 123456789,
+    };
+
+    let block = TangleBlock {
+        id: "block1".into(),
+        parents: vec!["parent1".into(), "parent2".into()]
